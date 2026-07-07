71012e0fb7e688a3a25d0aab738c9f36a74c7df7
Leela Prasad Pottangi <leelaprasadpottangi@gmail.com>
2026-07-07 16:16:13 +0530

    Day 3: Added Variables and Data Types programs

---
+++ 01-Java-Basics/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte age = 23;
+        short year = 2026;
+        int salary = 500000;
+        long population = 1450000000L;
+
+        float percentage = 89.5f;
+        double pi = 3.1415926535;
+
+        char grade = 'A';
+
+        boolean isPlaced = false;
+
+        System.out.println("Byte : " + age);
+        System.out.println("Short : " + year);
+        System.out.println("Int : " + salary);
+        System.out.println("Long : " + population);
+        System.out.println("Float : " + percentage);
+        System.out.println("Double : " + pi);
+        System.out.println("Char : " + grade);
+        System.out.println("Boolean : " + isPlaced);
+    }
+}
---
+++ 01-Java-Basics/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        String name = "Leela Prasad";
+        int age = 23;
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean placed = false;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("CGPA : " + cgpa);
+        System.out.println("Grade : " + grade);
+        System.out.println("Placed : " + placed);
+    }
+}
