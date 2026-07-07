f14631959b2d322419d39ea0633008313827b544
Leela Prasad Pottangi <leelaprasadpottangi@gmail.com>
2026-07-07 15:46:34 +0530

    Day 2: Added Hello World program

---
+++ 01-Java-Basics/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, GitHub!");
+        System.out.println("My name is Leela Prasad.");
+        System.out.println("This is Day 2 of Java-Mastery.");
+
+    }
+}
